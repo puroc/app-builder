fd8267f4e0c156edff0ff92c7928d58748e2ca58
puroc <p00u00r00>
2018-04-25 09:07:06 +0800

    1

--- src/views/test3/components/components.vue
+++ src/views/test3/components/components.vue
@@ -2,42 +2,18 @@
   <div @dragover="allowDrop" @dragstart="drag">
     <!-- {{content}}
     <el-input v-model="content" placeholder="请输入内容" draggable="true" id="input-currentTime"></el-input> -->
-    <div draggable="true" id="select-currentTime">
-      <el-select v-model="value" placeholder="请选择">
-        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
-        </el-option>
-      </el-select>
-    </div>
+    <SelectPlus/>
+
   </div>
 </template>
 <script>
+import SelectPlus from './selectPlus'
 export default {
+  components: {
+    SelectPlus
+  },
   data() {
     return {
-      content: '',
-      options: [
-        {
-          value: '选项1',
-          label: '黄金糕'
-        },
-        {
-          value: '选项2',
-          label: '双皮奶'
-        },
-        {
-          value: '选项3',
-          label: '蚵仔煎'
-        },
-        {
-          value: '选项4',
-          label: '龙须面'
-        },
-        {
-          value: '选项5',
-          label: '北京烤鸭'
-        }
-      ],
-      value: ''
     }
   },
   methods: {
